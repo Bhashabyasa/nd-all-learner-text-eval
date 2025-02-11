06ed6e16f535f3ca87d6768408dec74c5ed5d1e9
SethuK-ND <sethu@navadhiti.com>
2025-02-11 14:26:04 +0530

    updated code

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import os
-from dotenv import load_dotenv
 from fastapi import FastAPI
 from routes import router
 
-load_dotenv()
-
-app = FastAPI(docs_url="/api/docs", openapi_url="/api/openapi.json")
+app = FastAPI(
+    docs_url='/api/docs',
+    openapi_url='/api/openapi.json'
+)
 
 app.include_router(router)
 
@@ -19,6 +19,5 @@ async def health_check():
 
 if __name__ == "__main__":
     import uvicorn
-
     num_workers = os.cpu_count() or 1
-    uvicorn.run("app:app", host="0.0.0.0", port=APP_PORT, debug=False, workers=num_workers)
+    uvicorn.run("app:app", host="0.0.0.0", port=5001, debug=False, workers=num_workers)
